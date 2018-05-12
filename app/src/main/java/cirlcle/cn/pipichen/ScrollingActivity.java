1d1acf4dc612435741a4464f97682bfc597ec5f0
chenhuiming <hm348081>
2018-05-12 11:04:59 +0800

    圆点轮播

--- app/src/main/java/cirlcle/cn/pipichen/ScrollingActivity.java
+++ app/src/main/java/cirlcle/cn/pipichen/ScrollingActivity.java
@@ -1,15 +1,26 @@
 package cirlcle.cn.pipichen;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SimpleTimeZone;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    private LinearLayout mLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,8 @@ public class ScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mLinearLayout = (LinearLayout) findViewById(R.id.yuandian);
+        initYuanDian();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +39,12 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mYuandianHandler.removeCallbacks(mYuandianRunable);
     }
 
     @Override
@@ -48,4 +67,62 @@ public class ScrollingActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    private final int SIZE = 3;
+
+    /**
+     * 获取数据
+     */
+    private void initYuanDian() {
+        //设置图片
+        ImageView imageView;
+        View view;
+        for(int index = 0; index < SIZE; index++) {
+            //创建底部指示器(小圆点)
+            view = new View(this);
+            view.setBackgroundResource(R.drawable.background);
+            view.setEnabled(false);
+            //设置宽高
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(30, 30);
+            //设置间隔
+            if (index != 0) {
+                layoutParams.leftMargin = 10;
+            }
+            //添加到LinearLayout
+            mLinearLayout.addView(view, layoutParams);
+
+        }
+
+        mYuandianHandler.post(mYuandianRunable);
+    }
+
+    Handler mYuandianHandler = new Handler(Looper.myLooper());
+    private int mIndex = 0;
+
+
+    Runnable mYuandianRunable = new Runnable() {
+        @Override
+        public void run() {
+            setIndex(mIndex);
+            if(++mIndex == 3) {
+                mIndex = 0;
+            }
+            mYuandianHandler.removeCallbacks(mYuandianRunable);
+            mYuandianHandler.postDelayed(mYuandianRunable, 1000);
+        }
+    };
+
+    private void setIndex(int select) {
+        for(int index = 0; index< SIZE; index++) {
+            if(index == select) {
+                mLinearLayout.getChildAt(index).setEnabled(true);
+            } else {
+                mLinearLayout.getChildAt(index).setEnabled(false);
+            }
+        }
+    }
+
+
 }
